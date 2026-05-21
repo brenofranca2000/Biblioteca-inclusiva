77d74af3780dc5d8827a1206b423fdd71c5f3a47
brenofranca2000 <brenofranca2000@gmail.com>
2026-05-20 21:05:13 -0300

    feat: finalizei o CRUD de usuários com endpoints POST, GET, PUT e DELETE integrados ao PostgreSQL e Swagger

--- biblioteca inclusiva/src/main/java/org/serratec/biblioteca_inclusiva/controller/UsuarioController.java
+++ biblioteca inclusiva/src/main/java/org/serratec/biblioteca_inclusiva/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import org.serratec.biblioteca_inclusiva.dto.UsuarioResponseDTO;
 import org.serratec.biblioteca_inclusiva.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -18,4 +19,28 @@ public class UsuarioController {
 
         return usuarioService.salvar(dto);
     }
-}
+
+    @GetMapping
+    public List<UsuarioResponseDTO> listar() {
+
+        return usuarioService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioResponseDTO buscarPorId(@PathVariable Long id) {
+
+        return usuarioService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public UsuarioResponseDTO atualizar(@PathVariable Long id,
+                                        @RequestBody UsuarioRequestDTO dto) {
+
+        return usuarioService.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+
+        usuarioService.deletar(id);
+    }}
--- biblioteca inclusiva/src/main/java/org/serratec/biblioteca_inclusiva/service/UsuarioService.java
+++ biblioteca inclusiva/src/main/java/org/serratec/biblioteca_inclusiva/service/UsuarioService.java
@@ -6,6 +6,9 @@ import org.serratec.biblioteca_inclusiva.entity.Usuario;
 import org.serratec.biblioteca_inclusiva.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UsuarioService {
@@ -14,6 +17,7 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
 
     public UsuarioResponseDTO salvar(UsuarioRequestDTO dto) {
+
 
         Usuario usuario = new Usuario();
 
@@ -30,4 +34,70 @@ public class UsuarioService {
 
         return response;
     }
+
+    public List<UsuarioResponseDTO> listar() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+        return usuarios.stream().map(usuario -> {
+            UsuarioResponseDTO response = new UsuarioResponseDTO();
+            response.setId(usuario.getId());
+            response.setNome(usuario.getNome());
+            response.setEmail(usuario.getEmail());
+            return response;
+        }).collect(Collectors.toList());
+    }
+
+    public UsuarioResponseDTO buscarPorId(Long id) {
+
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+        if (usuarioOptional.isEmpty()) {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+
+        Usuario usuario = usuarioOptional.get();
+
+        UsuarioResponseDTO response = new UsuarioResponseDTO();
+
+        response.setId(usuario.getId());
+        response.setNome(usuario.getNome());
+        response.setEmail(usuario.getEmail());
+
+        return response;
+    }
+
+public UsuarioResponseDTO atualizar(Long id, UsuarioRequestDTO dto) {
+
+    Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+    if (usuarioOptional.isEmpty()) {
+        throw new RuntimeException("Usuário não encontrado");
+
+    }
+
+    Usuario usuario = usuarioOptional.get();
+
+    usuario.setNome(dto.getNome());
+    usuario.setEmail(dto.getEmail());
+
+    usuario = usuarioRepository.save(usuario);
+
+    UsuarioResponseDTO response = new UsuarioResponseDTO();
+
+    response.setId(usuario.getId());
+    response.setNome(usuario.getNome());
+    response.setEmail(usuario.getEmail());
+
+    return response;
 }
+
+public void deletar(Long id) {
+
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+        if (usuarioOptional.isEmpty()) {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+
+        usuarioRepository.deleteById(id);
+    }}
