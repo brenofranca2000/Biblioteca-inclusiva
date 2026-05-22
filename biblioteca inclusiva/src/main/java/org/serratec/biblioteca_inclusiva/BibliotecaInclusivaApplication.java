6d026f42cc84acd8734f0b1aa9e163aba3920b78
brenofranca2000 <brenofranca2000@gmail.com>
2026-05-22 18:20:58 -0300

    projeto completo

---
+++ biblioteca inclusiva/src/main/java/org/serratec/biblioteca_inclusiva/BibliotecaInclusivaApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.biblioteca_inclusiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaInclusivaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaInclusivaApplication.class, args);
+    }
+
+}
---
+++ biblioteca inclusiva/src/test/java/org/serratec/biblioteca_inclusiva/BibliotecaInclusivaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.biblioteca_inclusiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaInclusivaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
